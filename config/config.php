577fc18386786d980535fc87dbd576e7245d1840
crcms <crcms@crcms.cn>
2018-09-07 10:46:40 +0800

    update

--- config/config.php
+++ config/config.php
@@ -1,9 +1,6 @@
 <?php
 
 return [
-    'domain' => 'passport.crcms.local',
-    'api_domain' => '',//passport.api.crcms.local
-
     /*
     |--------------------------------------------------------------------------
     | JWT time to live
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::namespace('CrCms\Passport\Http\Web\Controllers')
-    ->group(function () {
-        Route::get('login', 'AuthController@getLogin')->name('login');
-        Route::get('register', 'AuthController@getRegister')->name('register');
-    });
+Route::namespace('CrCms\Passport\Http\Web\Controllers')->group(function () {
+    Route::get('login', 'AuthController@getLogin')->name('login');
+    Route::get('register', 'AuthController@getRegister')->name('register');
+});
