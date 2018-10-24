968ad42983bffcbb17a95f5b74e313bb5c56aabc
simon <434730525@qq.com>
2018-10-24 16:53:33 +0800

    Database structure modification

--- database/migrations/2018_08_11_085120_create_passport_user_table.php
+++ database/migrations/2018_08_11_085120_create_passport_user_table.php
@@ -15,18 +15,18 @@ class CreatePassportUserTable extends Migration
     {
         Schema::create('passport_users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name', 20)->commet('用户名');
+            $table->string('name', 20)->nullable()->commet('用户名');
             $table->string('nickname', 30)->nullable()->comment('昵称');
             $table->string('password', 150)->commet('密码');
             $table->string('email', 50)->nullable()->commet('email');
-            $table->string('mobile_phone', 15)->nullable()->commet('手机号');
+            $table->string('mobile', 15)->nullable()->commet('手机号');
             $table->unsignedTinyInteger('status')->default(0)->commet('状态，1正常，2未激活，3禁止');
             $table->unsignedBigInteger('created_at')->default(0)->comment('创建时间');
             $table->unsignedBigInteger('updated_at')->default(0)->comment('修改时间');
             $table->unsignedBigInteger('deleted_at')->default(null)->nullable()->comment('删除时间');
             $table->string('remember_token', 150)->nullable()->comment('remember token');
             $table->ipAddress('register_ip')->nullable()->commnet('Register IpAddress');
-            $table->unsignedInteger('register_application_id')->default(0)->comment('注册的所属应用来源');
+            $table->unsignedInteger('app_id')->default(0)->comment('注册的所属应用来源');
 
             $table->unique('name');
             $table->unique('mobile_phone');
