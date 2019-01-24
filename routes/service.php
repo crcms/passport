e2aba64e6ef2374a6a46e228758286cf11c12af3
crcms <crcms@crcms.cn>
2019-01-24 17:06:43 +0800

    clear

--- routes/service.php
+++ routes/service.php
@@ -6,7 +6,3 @@ Route::namespace('CrCms\Passport\Controllers')->group(function () {
     Route::register('auth', 'AuthController', ['only' => ['login', 'check', 'refresh', 'user', 'register']]);
     Route::register('user', 'UserController', ['only' => ['index', 'store', 'update', 'destroy']]);
 });
-
-//Route::register('login','AuthController@login');
-//Route::register('login','AuthController@login');
-//Route::register('login','AuthController@login');
