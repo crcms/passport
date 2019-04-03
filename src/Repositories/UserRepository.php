bffc86d5107496a3df92b1a1861abb60cf30fb21
crcms <crcms@crcms.cn>
2019-04-03 15:00:43 +0800

    fix: status update invalid

--- src/Repositories/UserRepository.php
+++ src/Repositories/UserRepository.php
@@ -22,7 +22,7 @@ class UserRepository extends AbstractRepository
     /**
      * @var array
      */
-    protected $guard = ['name', 'email', 'password', 'mobile'];
+    protected $guard = ['name', 'email', 'password', 'status', 'mobile'];
 
     /**
      * @return UserModel
